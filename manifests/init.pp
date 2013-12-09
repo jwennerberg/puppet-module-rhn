8e4427a1eb027f0af7da9796bd2c0131057ace05
Daniel Tremblay <github@danieltremblay.ca>
2013-12-09 21:42:42 +0000

    Add support for CentOS so it doesn't run on this lsbdistid

--- manifests/init.pp
+++ manifests/init.pp
@@ -19,55 +19,56 @@ class rhn (
   $up2date_ssl_ca_cert  = '/usr/share/rhn/RHN-ORG-TRUSTED-SSL-CERT',
 ) {
 
-  case $::osfamily {
-    'RedHat': { }
+  case $::lsbdistid {
+    /RedHat/: {
+      $rhnsd_service_enable_type = type($rhnsd_service_enable)
+      if $rhnsd_service_enable_type == 'string' {
+        $rhnsd_service_enabled = str2bool($rhnsd_service_enable)
+      } else {
+        $rhnsd_service_enabled = $rhnsd_service_enable
+      }
+
+      validate_re($rhnsd_service_ensure, '^(running|stopped)$', 'rhn::rhnsd_service_ensure must be set to either running or stopped')
+      validate_bool($rhnsd_service_enabled)
+      validate_absolute_path($rhnsd_file_path)
+      validate_absolute_path($up2date_file_path)
+      validate_absolute_path($up2date_ssl_ca_cert)
+
+      package { 'rhn_packages':
+        ensure => 'installed',
+        name   => $packages,
+      }
+
+      file { 'up2date_file':
+        ensure  => 'file',
+        path    => $up2date_file_path,
+        owner   => $up2date_file_owner,
+        group   => $up2date_file_group,
+        mode    => $up2date_file_mode,
+        content => template('rhn/up2date.erb'),
+        require => Package['rhn_packages'],
+      }
+
+      file { 'rhnsd_file':
+        ensure  => 'file',
+        path    => $rhnsd_file_path,
+        owner   => $rhnsd_file_owner,
+        group   => $rhnsd_file_group,
+        mode    => $rhnsd_file_mode,
+        content => template('rhn/rhnsd.erb'),
+        require => Package['rhn_packages'],
+        notify  => Service['rhnsd_service'],
+      }
+
+      service { 'rhnsd_service':
+        ensure  => $rhnsd_service_ensure,
+        enable  => $rhnsd_service_enabled,
+        require => Package['rhn_packages'],
+      }
+    }
     default: {
-      fail("rhn is supported on osfamily RedHat. Your osfamily identified as ${::osfamily}.")
+      notice("rhn is supported on osfamily RedHat and lsbdistid RedHat. Your osfamily identified as ${::osfamily} and your lsbdistid is identified as ${::lsbdistid}.")
     }
   }
 
-  $rhnsd_service_enable_type = type($rhnsd_service_enable)
-  if $rhnsd_service_enable_type == 'string' {
-    $rhnsd_service_enabled = str2bool($rhnsd_service_enable)
-  } else {
-    $rhnsd_service_enabled = $rhnsd_service_enable
-  }
-
-  validate_re($rhnsd_service_ensure, '^(running|stopped)$', 'rhn::rhnsd_service_ensure must be set to either running or stopped')
-  validate_bool($rhnsd_service_enabled)
-  validate_absolute_path($rhnsd_file_path)
-  validate_absolute_path($up2date_file_path)
-  validate_absolute_path($up2date_ssl_ca_cert)
-
-  package { 'rhn_packages':
-    ensure => 'installed',
-    name   => $packages,
-  }
-
-  file { 'up2date_file':
-    ensure  => 'file',
-    path    => $up2date_file_path,
-    owner   => $up2date_file_owner,
-    group   => $up2date_file_group,
-    mode    => $up2date_file_mode,
-    content => template('rhn/up2date.erb'),
-    require => Package['rhn_packages'],
-  }
-
-  file { 'rhnsd_file':
-    ensure  => 'file',
-    path    => $rhnsd_file_path,
-    owner   => $rhnsd_file_owner,
-    group   => $rhnsd_file_group,
-    mode    => $rhnsd_file_mode,
-    content => template('rhn/rhnsd.erb'),
-    require => Package['rhn_packages'],
-    notify  => Service['rhnsd_service'],
-  }
-
-  service { 'rhnsd_service':
-    ensure  => $rhnsd_service_ensure,
-    enable  => $rhnsd_service_enabled,
-    require => Package['rhn_packages'],
-  }
 }
